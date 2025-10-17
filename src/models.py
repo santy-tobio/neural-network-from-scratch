62e6239b1cce6b1f4978bfc62615a67a9100d19a
santy-tobio <stobio@udesa.edu.ar>
2025-10-17 12:46:35 -0300

    MLP casi terminado

--- src/models.py
+++ src/models.py
@@ -1,28 +1,60 @@
 import numpy as np
 import pandas as pd
 import cupy as cp
+from enum import Enum
 
-# layer(input) -> output
-# -
-# -
-# neural_net(layers,)
-# - eval(input) -> output
-# - loss(predicted, actual) -> loss
 
-# Layer
-# Linear(Layer)
-# Relu(Layer)
-# [Linear, Relu, Linear, Softmax]
+class MLP:
+    def __init__(
+        self,
+        batch_size: int,
+        input_dim: int,
+        output_classes: int,
+        hidden_layers_neuron_count: list[int],
+    ):
+        # Architecture parameters
+        self.batch_size = batch_size
+        self.layers = []
+        # [input, ..., hidden_layers] -> then [hidden_layer[-1], output_classes]
+        self.relu_layers = [input_dim] + hidden_layers_neuron_count
+        # Append all relu layers
+        for layer in range(0, (len(self.relu_layers))):
+            self.layers += [
+                Linear(
+                    self.relu_layers[layer],
+                    self.relu_layers[layer + 1],
+                    batch_size,
+                ),
+                Relu(),
+            ]
+        # Append last layer (Softmax)
+        self.layers += [
+            Linear(self.relu_layers[-1], output_classes, batch_size),
+            CESoftmax(),
+        ]
 
-# Layer
-# Linear(Layer)
-# Relu(Linear)
-# [Relu, Relu, Softmax]
+    def forward(self, x: cp.ndarray) -> cp.ndarray:
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
 
-# LinearLayer
-# Relu(LinearLayer)
-# Softmax(LinearLayer)
-# [Relu, Relu, Softmax]
+    def backward(self, y: cp.ndarray) -> cp.ndarray:
+        prev_grad = y
+        for layer in reversed(self.layers):
+            prev_grad = layer.backward(prev_grad)
+        return prev_grad
+
+    def update_weights(self, learning_rate: float):
+        for layer in self.layers:
+            if isinstance(layer, Linear):
+                layer.weights -= learning_rate * layer.grad_weights
+                layer.bias -= learning_rate * layer.grad_bias
+
+    def loss(self, x: cp.ndarray) -> cp.float:
+        return cp.mean(-cp.log(cp.clip(x, 1e-10, 1.0)))
+
+    # def save_weights(self):
+    # def load_weights(self, model ):
 
 
 class Layer:
@@ -38,18 +70,18 @@ class Linear(Layer):
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.batch_size = batch_size
-        sigma = cp.sqrt(2.0 / (input_dim + output_dim))
+
+        # TODO: Merge bias as Weight 0
+        self.weights = cp.random.randn(output_dim, input_dim).astype(
+            cp.float32
+        ) * cp.sqrt(2.0 / (input_dim + output_dim))
+        # Bias as a colum vector [output_dim, batch_size]
+        self.bias = cp.zeros((output_dim, batch_size), dtype=cp.float32)
 
         # cache para backward
         self.input = cp.zeros((input_dim, batch_size), dtype=cp.float32)
         self.grad_weights = cp.zeros((output_dim, input_dim), dtype=cp.float32)
         self.grad_bias = cp.zeros((output_dim, batch_size), dtype=cp.float32)
-
-        # TODO: Merge into weight0 = bias
-        self.weights = (
-            cp.random.randn((output_dim, input_dim), dtype=cp.float32) * sigma
-        )
-        self.bias = cp.zeros((output_dim, batch_size), dtype=cp.float32)
 
     def forward(self, x: cp.ndarray) -> cp.ndarray:
         # weight: (output_dim, input_dim) @ x: (input_dim, batch_size) -> (output_dim, batch_size)
@@ -59,38 +91,34 @@ class Linear(Layer):
         return self.weights @ x + self.bias
 
     def backward(self, prev_grad: cp.ndarray) -> cp.ndarray:
-        # Bias
-        self.grad_bias = cp.sum(prev_grad, axis=1, keepdims=True) / self.batch_size
-        self.grad_weights = prev_grad @ self.input.T
-        # Weights
-        return self.grad_weights.T @ prev_grad
+        # prev_grad: (output_dim, batch_size), input: (input_dim, batch_size)
+        # Weights: DL/DZ3 * (a^i-1)^T
+        self.grad_weights = prev_grad @ self.input.T  # (output_dim, input_dim)
+        # Bias gradient is the sum over the batch dimension -> (output_dim, 1)
+        self.grad_bias = cp.sum(prev_grad, axis=1, keepdims=True)
 
-
-class NeuralNetwork:
-    num_clases = 47
-    hidden_layers = 0
-    neurons_per_layer = 0
-    batch_size = 3
-    layers = []
-
-    def __init__(self):
-        self.layers = [Relu()]
-
-    def forward():
-        pass
-
-    def backward():
-        pass
+        # TODO: Why w.T * prev_grad and not w * prev_grad?
+        return self.weights.T @ prev_grad
 
 
 class Relu(Layer):
-    def forward(self, z_prev: cp.ndarray):
-        return cp.max(0, self.weights * z_prev + self.bias)
+    def forward(self, x: cp.ndarray):
+        self.mask = cp.array(x > 0, dtype=cp.float32)
+        return x * self.mask
 
-    def backward(self, output):
-        raise NotImplemented
+    def backward(self, prev_grad: cp.ndarray):
+        return prev_grad * self.mask  # prev_grad @ diag(self.mask)
 
 
-class Softmax(Layer):
-    def forward(self, input):
-        return input
+class CESoftmax(Layer):
+
+    def forward(self, logits: cp.ndarray):
+        # Subtract max for numerical stability
+        exp_input = cp.exp(logits - cp.max(logits, axis=0, keepdims=True))
+        # Returns only softmax output
+        self.output = exp_input / cp.sum(exp_input, axis=0, keepdims=True)
+        return self.output
+
+    def backward(self, prev_grad: cp.ndarray):
+        # TODO: Rightnow, the backward is the CrossEntropy + Softmax derivative (Not pure Softmax)
+        return self.output - prev_grad
